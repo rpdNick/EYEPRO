81ceb1607acc4faa090767f1b7cc915477db0b09
rpdNick <rpd_nick@ukr.net>
2025-09-12 00:44:15 +0300

    Lighting for clinics page

---
+++ js/slider_lighting.js
@@ -0,0 +1,43 @@
+function initResponsiveSlider() {
+  const $slider = $('.slider_lighting');
+
+  function getSlidesToShow() {
+    const containerWidth = $slider.width();
+    const minSlideWidth = 237;
+    const gap = 24;
+
+    let slidesToShow = Math.floor((containerWidth + gap) / (minSlideWidth + gap));
+
+    if (slidesToShow < 1) slidesToShow = 1;
+
+    if (containerWidth < 480) slidesToShow = 1.3;
+
+    return slidesToShow;
+  }
+
+  // Ініціалізація слайдера
+  $slider.slick({
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: getSlidesToShow(),
+    slidesToScroll: 1,
+  });
+
+  // При ресайзі перераховуємо slidesToShow
+  $(window).on('resize', function () {
+    const newSlidesToShow = getSlidesToShow();
+    $slider.slick('slickSetOption', 'slidesToShow', newSlidesToShow, true);
+  });
+}
+
+
+initResponsiveSlider();
+
+function refreshSlider() {
+  const $slider = $('.slider_lighting');
+  if ($slider.hasClass('slick-initialized')) {
+    $slider.slick('unslick');
+  }
+  initResponsiveSlider();
+}
